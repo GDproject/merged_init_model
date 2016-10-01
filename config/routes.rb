0b5e074f6080ec033d6221fb4c0fc1846a3ef006
ledinhdoan <ledinhdoan.0083@gmail.com>
2016-10-01 13:58:52 +0700

    login-logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "staticpage#index"
-
+  get '/new_session', to: "sessions#new"
+  post '/new_session', to: "sessions#create"
+  delete '/delete_session',  to: 'sessions#destroy'
   resources :users
+  resources :sessions
 end
